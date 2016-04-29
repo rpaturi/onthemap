4da926c80719e44365e3d4b1889aa36f541894ba
PatYourEye <rachelpaturi@gmail.com>
2016-04-29 14:50:16 -0700

    Added: JSON body constants to StudentInformation class

---
+++ On the Map/Constants .swift
@@ -0,0 +1,23 @@
+//
+//  Constants .swift
+//  On the Map
+//
+//  Created by Rachel Paturi on 4/29/16.
+//  Copyright © 2016 Rachel Paturi. All rights reserved.
+//
+
+import Foundation
+
+extension StudentInformation {
+
+    struct JSONBodyKeys {
+        static let uniqueKey = "uniqueKey"
+        static let firstName = "firstName"
+        static let lastName = "lastName"
+        static let mapString = "mapString"
+        static let mediaURL = "mediaURL"
+        static let latitude = "latitude"
+        static let longitude = "longitude "
+    }
+}
+
